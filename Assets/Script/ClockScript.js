6afe60735daa90e0fc2c78facc910e0f2f6a5f80
Rohail Najam <rohail_najam@hotmail.com>
2014-11-24 17:25:45 +0500

    Second Commit
    Chapt 7

---
+++ Assets/Script/ClockScript.js
@@ -0,0 +1,56 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var textvar:Text;
+textvar = GetComponent.<Text>();
+textvar.material.color = Color.black;
+var isPause : boolean = false;
+var startTime : float;
+var timeRemaining : float;
+
+function Start () {
+
+	startTime = 5.0;
+
+}
+
+function Update () {
+	
+	if(isPause)
+	{
+		DoCountDown();
+		
+	}
+
+}
+
+function DoCountDown()
+{
+
+	
+
+}
+
+function PauseClock()
+{
+
+	isPause = true;
+}
+
+function UnPauseClock()
+{
+	isPause = false;
+
+}
+
+function ShowTime()
+{
+
+	
+}
+function TimesUp()
+{
+
+	
+
+}
